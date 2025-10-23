3c421b693363b13f3cc16f091c7f889c864ac2c0
Syed Ata ur Rahaman <syedataurrahaman1502@gmail.com>
2025-10-23 05:49:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import Login from './Components/login';
 import Quiz from './Components/Quiz';
 import Feedback from './Components/feedback';
